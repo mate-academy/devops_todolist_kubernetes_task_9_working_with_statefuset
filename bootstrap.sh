16008f02ffceb00c1132ad067d61303b9d27cc7c
alexh <olexandrgauptf@gmail.com>
2024-04-03 23:54:12 +0300

    write command to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+kubectl create namespace mysql
+kubectl apply -f .infrastructure/namespace.yml
+
+kubectl apply -f .infrastructure/statefulSet.yml
+
+echo "Checking the created MySQL resources..."
+kubectl get all -n mysql
+
+
+kubectl apply -f .infrastructure/configMap.yml
+kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
+kubectl apply -f .infrastructure/clusterIp.yml
+kubectl apply -f .infrastructure/nodePort.yml
+kubectl apply -f .infrastructure/hpa.yml
+kubectl apply -f .infrastructure/deployment.yml
+
+echo "Checking the created todoapp resources..."
+kubectl get all -n todoapp
