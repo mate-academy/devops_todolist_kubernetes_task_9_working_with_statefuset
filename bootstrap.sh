c57ddf1703caf9b73e989d0d86cf1f92094f0abf
alexh <olexandrgauptf@gmail.com>
2024-04-05 17:41:25 +0300

    fix with mysql namespace

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@ kubectl apply -f .infrastructure/namespace.yml
 
 kubectl apply -f .infrastructure/statefulSet.yml
 
-echo "Checking the created MySQL resources..."
-kubectl get all -n mysql
+echo "Checking the created todoapp resources..."
+kubectl get all -n todoapp
 
 
 kubectl apply -f .infrastructure/configMap.yml
