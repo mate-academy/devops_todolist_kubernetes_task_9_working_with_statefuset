79dd84a961f3a3fc36be9d2e88050b29fae14862
Volodymyr Panov <vpanov25@gmail.com>
2024-05-12 07:21:22 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+kubectl apply -f namespace.yml
 kubectl apply -f st-service.yml
 kubectl apply -f st-secret.yml
 kubectl apply -f st-configMap.yml
 kubectl apply -f statefulSet.yml
 kubectl apply -f secret.yml
 kubectl apply -f deployment.yml
-kubectl apply -f namespace.yml
