d0d9ce3f1dfc04c420a7fdd0307f1e92558b5044
Volkov <volkovyegorandriiovich@gmail.com>
2024-06-20 06:30:35 +0300

    solution?

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+kind delete cluster
+kind create cluster --config .infrastructure/cluster.yml
+kubectl apply -f .\.infrastructure\namespace.yml
+kubectl apply -f .\.infrastructure\confgiMap.yml
+kubectl apply -f .\.infrastructure\secret.yml
+kubectl apply -f .\.infrastructure\clusterIp.yml
+kubectl apply -f .\.infrastructure\nodeport.yml
+kubectl apply -f .\.infrastructure\pv.yml
+kubectl apply -f .\.infrastructure\pvc.yml
+kubectl apply -f .\.infrastructure\statefulSet.yml
+kubectl apply -f .\.infrastructure\deployment.yml
+kubectl apply -f .\.infrastructure\hpa.yml
