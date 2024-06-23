3ecf291d7c21fff2daebcb1e49873070c81da5e1
Volkov <volkovyegorandriiovich@gmail.com>
2024-06-24 02:35:35 +0300

    separated app and db manifests

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# creating cluster
 kind delete cluster
 kind create cluster --config ./cluster.yml
+
+# crating database
+kubectl apply -f .infrastructure/mysql/namespace.yml
+kubectl apply -f .infrastructure/mysql/configMap.yml
+kubectl apply -f .infrastructure/mysql/secret.yml
+kubectl apply -f .infrastructure/mysql/service.yml
+kubectl apply -f .infrastructure/mysql/statefulSet.yml
+
+#creating app
 kubectl apply -f .\.infrastructure\namespace.yml
 kubectl apply -f .\.infrastructure\confgiMap.yml
 kubectl apply -f .\.infrastructure\secret.yml
@@ -9,6 +19,5 @@ kubectl apply -f .\.infrastructure\clusterIp.yml
 kubectl apply -f .\.infrastructure\nodeport.yml
 kubectl apply -f .\.infrastructure\pv.yml
 kubectl apply -f .\.infrastructure\pvc.yml
-kubectl apply -f .\.infrastructure\statefulSet.yml
 kubectl apply -f .\.infrastructure\deployment.yml
 kubectl apply -f .\.infrastructure\hpa.yml
