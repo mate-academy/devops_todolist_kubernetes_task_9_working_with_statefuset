cec510d51b33cae6f6d73bdb36e11fb292671fe9
Marko Panchak <marko.panchak1@gmail.com>
2024-07-20 20:37:30 +0300

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+kubectl apply -f .infrastructure/statefulNamespace.yml
+kubectl apply -f .infrastructure/statefulConfigMap.yml
+kubectl apply -f .infrastructure/statefulSecret.yml
+kubectl apply -f .infrastructure/statefulHeadlessService.yml
+kubectl apply -f .infrastructure/statefulSet.yml
+kubectl apply -f .infrastructure/namespace.yml
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
+kubectl apply -f .infrastructure/clusterIp.yml
+kubectl apply -f .infrastructure/nodeport.yml
+kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/confgiMap.yml
+kubectl apply -f .infrastructure/hpa.yml
+kubectl apply -f .infrastructure/deployment.yml
