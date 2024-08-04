e888957f2b950148506325498d8c8667b71f0dbe
elfredburger <elfredfredburger@gmail.com>
2024-08-04 19:18:28 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+cd .infrastructure
+kubectl create ns mysql
+kubectl apply -f st-service.yml
+kubectl apply -f st-secret.yml
+kubectl apply -f st-configMap.yml
+kubectl apply -f statefullSet.yml
