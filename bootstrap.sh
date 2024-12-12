d8ae3bedcec59ec154eb2e554d66cd4716e9d35e
Victor Rudkovsky <vpcomtek@gmail.com>
2024-12-13 01:33:34 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ kubectl apply -f .infrastructure/namespace.yml
 kubectl create namespace mysql
 
 # Apply MySQL StatefulSet and ConfigMap
-kubectl apply -f manifests/mysql/mysql-secret.yml
-kubectl apply -f manifests/mysql/init-configMap.yml
-kubectl apply -f manifests/mysql/mysql-headless-service.yml
-kubectl apply -f manifests/mysql/statefulSet.yml
+kubectl apply -f .infrastructure/mysql/mysql-secret.yml
+kubectl apply -f .infrastructure/mysql/init-configMap.yml
+kubectl apply -f .infrastructure/mysql/mysql-headless-service.yml
+kubectl apply -f .infrastructure/mysql/statefulSet.yml
 
 #Create todoaapp and services
 kubectl apply -f .infrastructure/pv.yml
